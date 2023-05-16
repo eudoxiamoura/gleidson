a6a4167a4feabf1d234c1fa90227d397dbe17cff
eudoxiamoura <103461982+eudoxiamoura@users.noreply.github.com>
2023-05-16 15:06:25 -0400

    16052023
    16052023

--- folder.py
+++ folder.py
@@ -1,17 +1,16 @@
 import os
 import glob
-import cv2
+
+
 def getFiles(database, ext):
-  
-  path = database + '/*.' + ext
-  files = glob.glob(path)
-  """
+    path = database+'*.'+ext
+    files = glob.glob(path)
+    filesorted = sorted(files)
+
+    """
   files = []
   for n in os.listdir(database):
     if(not os.path.isdir(database+n)):
       files.append(database+n)
   """
-  return files
-
-img_paths = getFiles('Processamento/Result/Real', 'jpg')
-print(img_paths)
+    return filesorted
